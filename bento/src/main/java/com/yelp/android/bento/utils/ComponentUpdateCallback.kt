ed08cfe3ae7e6839f22e67c8b2af29443e0e47fc
Steven Meliopoulos <smeliopo@yelp.com>
2018-09-04 11:42:54 -0700

    Adapter for using DiffUtil on bento components
    DiffUtil lets you easily dispatch precise updates to a RecyclerView. This adds
    a small adapter so it can be used directly on Bento components too.
    https://developer.android.com/reference/android/support/v7/util/DiffUtil
    
    https://reviewboard.yelpcorp.com/r/334529/

---
+++ bento/src/main/java/com/yelp/android/bento/utils/ComponentUpdateCallback.kt
@@ -0,0 +1,28 @@
+package com.yelp.android.bento.utils
+
+import android.support.v7.util.ListUpdateCallback
+import com.yelp.android.bento.core.Component
+
+/**
+ * An adapter to use [DiffUtil.dispatchUpdatesTo()] on a Bento component.
+ * Usage:
+ * val diff = DiffUtil.calculateDiff(...)
+ * diff.dispatchUpdatesTo(ComponentUpdateCallback(yourComponent))
+ */
+class ComponentUpdateCallback(val component: Component) : ListUpdateCallback {
+    override fun onChanged(position: Int, count: Int, payload: Any?) {
+        component.notifyItemRangeChanged(position, count)
+    }
+
+    override fun onMoved(fromPosition: Int, toPosition: Int) {
+        component.notifyItemMoved(fromPosition, toPosition)
+    }
+
+    override fun onInserted(position: Int, count: Int) {
+        component.notifyItemRangeInserted(position, count)
+    }
+
+    override fun onRemoved(position: Int, count: Int) {
+        component.notifyItemRangeRemoved(position, count)
+    }
+}
