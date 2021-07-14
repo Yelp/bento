974113f58f64e9e0c8844157628717cc6537d6a0
Benoit Vermont <bvermont@yelp.com>
2021-07-14 15:44:53 +0200

    Let's use the existing guava method

--- bento/src/main/java/com/yelp/android/bento/utils/EqualsHelper.kt
+++ bento/src/main/java/com/yelp/android/bento/utils/EqualsHelper.kt
@@ -1,36 +1,16 @@
 package com.yelp.android.bento.utils
 
-import kotlin.reflect.KClass
-import kotlin.reflect.KProperty1
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.isAccessible
+import org.apache.commons.lang3.builder.EqualsBuilder
 
 object EqualsHelper {
+    // Works well enough, but not for collections.
     fun Any?.deepEquals(other: Any?): Boolean {
-        if (this == null && other == null) {
-            return true
-        }
-        if (this == null || other == null) {
-            return false
-        }
-
-        if (this::class != other::class) return false
-
-        val kClass: KClass<out Any> = this::class
-        if (kClass.java.isPrimitive || kClass.isData) {
-            return this == other
-        }
-
-        return kClass.memberProperties.all { member ->
-            @Suppress("UNCHECKED_CAST")
-            val castedMember = member as KProperty1<Any, *>
-            val wasAccessible = castedMember.isAccessible
-            castedMember.isAccessible = true
-            val memberA = castedMember.get(this)
-            val memberB = castedMember.get(other)
-            castedMember.isAccessible = wasAccessible
-
-            memberA.deepEquals(memberB)
+        return when {
+            this == null && other == null -> true
+            this == null || other == null -> false
+            this::class != other::class -> false
+            this::class.isData -> this == other
+            else -> EqualsBuilder.reflectionEquals(this, other)
         }
     }
 }
